34890cdf397bf788864680e88330ebf6a2bf5144
Mellisa0726 <61628518+Mellisa0726@users.noreply.github.com>
2022-08-15 01:42:01 +0800

    finish RecipeScreen

--- screens/EatScreen.tsx
+++ screens/EatScreen.tsx
@@ -1,43 +1,49 @@
 import React, { Component } from 'react';
-import { ScrollView, StyleSheet, View, Text, Image } from 'react-native';
+import { ScrollView, SafeAreaView, StyleSheet, View, Text, Image } from 'react-native';
 
 export default class EatScreen extends Component {
     render() {
         return (
-            <View style={styles.container}>
-                <ScrollView>
-                    <Text style={styles.border}/>
-                    <Image style={styles.center} source={require('../assets/images/香蕉色卡.jpeg')} />
-                    <Text style={styles.border}/>
-                    <Image style={styles.banana} source={require('../assets/images/banana_1.png')} />
-                    <Text style={styles.header}/>
-                    <Text style={styles.header}> • 墨綠色{"\n"}</Text>
-                    <Text style={styles.text}>
-                    未熟，不宜食用{"\n"}
-                    </Text>
-                    <Image style={styles.banana} source={require('../assets/images/banana_2.png')} />
-                    <Text style={styles.header} />
-                    <Text style={styles.header}> • 淡青色至半青半黃{"\n"}</Text>
-                    <Text style={styles.text}>
-                    口感苦澀、皮厚肉硬，含有「難消化性麥芽糊精」，不易被小腸吸收，升糖指數較低，可被腸道細菌發酵，具有穩定血糖之作用，有助於改善腸道健康。具有抗性澱粉、提供高飽足感，但不易消化，吃多容易引起腹脹、消化不良等反應。{"\n"}
-                    </Text>
-                    <Image style={styles.banana} source={require('../assets/images/banana_3.png')} />
-                    <Text style={styles.header} />
-                    <Text style={styles.header}> • 四分之三至全黃香蕉{"\n"}</Text>
-                    <Text style={styles.text}>
-                    味道香濃、口感軟滑，含有豐富維他命B2、B6及C，能促進新陳代謝，具養顏美白效果。此階段香蕉中含有豐富的鉀和鎂，能消除疲勞，減少運動時抽筋的機會。助消化、安眠、抗憂鬱。{"\n"}
-                    </Text>
-                    <Image style={styles.banana} source={require('../assets/images/banana_4.png')} />
-                    <Text style={styles.header} />
-                    <Text style={styles.header}> • 帶有啡點之全熟香蕉{"\n"}</Text>
-                    <Text style={styles.text}>
-                    口感軟爛，適合牙口不好者食用，含有大量多酚，能夠延緩衰老，研究顯示啡點越多，免疫活性越高，當中的磷脂質能有助抑制胃潰瘍，幫助消化。{"\n"}
-                    </Text>
-                    <Text style={styles.border} />
-                    <Image style={styles.center} source={require('../assets/images/香蕉色卡.jpeg')} />
-                    <Text style={styles.border} />
-                </ScrollView>
-            </View>
+            <SafeAreaView style={{ flex: 1 }}>
+                <View style={styles.container}>
+                    <ScrollView>
+                        <Text style={styles.border} />
+                        <Image style={styles.center} source={require('../assets/images/香蕉色卡.jpeg')} />
+                        <Text style={styles.border} />
+                        <View style={styles.middle}>
+                            <Text style={styles.header} />
+                            <Image style={styles.banana} source={require('../assets/images/banana_1.png')} />
+                            <Text style={styles.header} />
+                            <Text style={styles.header}> • 墨綠色{"\n"}</Text>
+                            <Text style={styles.text}>
+                                未熟，不宜食用{"\n"}
+                            </Text>
+
+                            <Image style={styles.banana} source={require('../assets/images/banana_2.png')} />
+                            <Text style={styles.header} />
+                            <Text style={styles.header}> • 淡青色至半青半黃{"\n"}</Text>
+                            <Text style={styles.text}>
+                                口感苦澀、皮厚肉硬，含有「難消化性麥芽糊精」，不易被小腸吸收，升糖指數較低，可被腸道細菌發酵，具有穩定血糖之作用，有助於改善腸道健康。具有抗性澱粉、提供高飽足感，但不易消化，吃多容易引起腹脹、消化不良等反應。{"\n"}
+                            </Text>
+                            <Image style={styles.banana} source={require('../assets/images/banana_3.png')} />
+                            <Text style={styles.header} />
+                            <Text style={styles.header}> • 四分之三至全黃香蕉{"\n"}</Text>
+                            <Text style={styles.text}>
+                                味道香濃、口感軟滑，含有豐富維他命B2、B6及C，能促進新陳代謝，具養顏美白效果。此階段香蕉中含有豐富的鉀和鎂，能消除疲勞，減少運動時抽筋的機會。助消化、安眠、抗憂鬱。{"\n"}
+                            </Text>
+                            <Image style={styles.banana} source={require('../assets/images/banana_4.png')} />
+                            <Text style={styles.header} />
+                            <Text style={styles.header}> • 帶有啡點之全熟香蕉{"\n"}</Text>
+                            <Text style={styles.text}>
+                                口感軟爛，適合牙口不好者食用，含有大量多酚，能夠延緩衰老，研究顯示啡點越多，免疫活性越高，當中的磷脂質能有助抑制胃潰瘍，幫助消化。{"\n"}
+                            </Text>
+                        </View>
+                        <Text style={styles.border} />
+                        <Image style={styles.center} source={require('../assets/images/香蕉色卡.jpeg')} />
+                        <Text style={styles.border} />
+                    </ScrollView>
+                </View>
+            </SafeAreaView>
         )
     }
 }
@@ -45,32 +51,32 @@ export default class EatScreen extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor:'white',
-        justifyContent: 'center',
+        backgroundColor:'#fff',
         alignItems: 'center',
     },
     border: {
         flex: 1,
         fontSize: 5,
-        backgroundColor: 'black',
-        justifyContent: 'center',
-        alignItems: 'center',
+        backgroundColor: '#000',
     },
     center: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
         width:420,
-        height:27
+        height:27,
+    },
+    middle:{
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 0,
     },
     banana:{
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
         borderRadius: 50,
-        width: 300,
+        width: "70%",
         height: 200,
-        //marginVertical:'center',
+        marginLeft: -17,
     },
     header: {
         fontSize: 20,
--- screens/RecipeScreen.tsx
+++ screens/RecipeScreen.tsx
@@ -1,26 +1,119 @@
-import React, { Component } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React from 'react';
+import {
+    View,
+    StyleSheet,
+    ScrollView,
+    Image,
+    Animated,
+    Dimensions,
+    SafeAreaView
+} from 'react-native';
 
-export default class RecipeScreen extends Component {
+import RNAnimatedScrollIndicators from '../node_modules/react-native-animated-scroll-indicators';
+
+const { width } = Dimensions.get('window');
+
+class Example2 extends React.Component {
+    scrollX = new Animated.Value(0);
+
     render() {
         return (
-            <View style={styles.container}>
-                <View style={styles.pages}/>
-            </View>
-        )
+            <SafeAreaView style={{ flex: 1 }}>
+                <View style={{ flex: 1 }}>
+                    <Animated.ScrollView
+                        horizontal
+                        pagingEnabled
+                        showsHorizontalScrollIndicator={false}
+                        contentContainerStyle={{ flexGrow: 1 }}
+                        onScroll={Animated.event(
+                            [{ nativeEvent: { contentOffset: { x: this.scrollX } } }],
+                            { useNativeDriver: true })}
+                    >
+                        <ScrollView>
+                            <View style={styles.container}>
+                                <Image style={styles.pic1} source={require('../assets/images/烤香蕉片.png')} />
+                            </View>
+                        </ScrollView>
+                        <ScrollView>
+                            <View style={styles.container}>
+                                <Image style={styles.pic2} source={require('../assets/images/香蕉鬆餅.png')} />
+                            </View>
+                        </ScrollView>
+                        <ScrollView>
+                            <View style={styles.container}>
+                                <Image style={styles.pic3} source={require('../assets/images/香蕉蛋糕.png')} />
+                            </View>
+                        </ScrollView>
+                        <ScrollView>
+                            <View style={styles.container}>
+                                <Image style={styles.pic4} source={require('../assets/images/香蕉奶昔.png')} />
+                            </View>
+                        </ScrollView>
+                        <ScrollView>
+                            <View style={styles.container}>
+                                <Image style={styles.pic5} source={require('../assets/images/香蕉餅乾.png')} />
+                            </View>
+                        </ScrollView>
+                    </Animated.ScrollView>
+                    <View style={{
+                        left: 0,
+                        right: 0,
+                        bottom: 0,
+                        zIndex: 100,
+                        marginBottom: 20,
+                        position: 'absolute'
+                    }}>
+                        <RNAnimatedScrollIndicators
+                            numberOfCards={7}
+                            scrollWidth={width}
+                            activeColor={'#FF8000'}
+                            inActiveColor={'#BEBEBE'}
+                            scrollAnimatedValue={this.scrollX}
+                        />
+                    </View>
+                </View>
+            </SafeAreaView>
+        );
     }
 }
+
+export default Example2
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        color: "#fff",
+        backgroundColor: "#fff",
+        alignItems: "center",
+        width
     },
-    pages: {
-        height: 500,
-        width: 320,
-        backgroundColor: 'green'
+    pic1: {
+        flex: 1,
+        resizeMode: 'contain',
+        width: "80%",
+        marginTop: -10,
     },
-})
+    pic2: {
+        flex: 1,
+        resizeMode: 'contain',
+        width: "80%",
+        marginTop: -12,
+    },
+    pic3: {
+        flex: 1,
+        resizeMode: 'contain',
+        width: "80%",
+        marginTop: -26,
+    },
+    pic4: {
+        flex: 1,
+        resizeMode: 'contain',
+        width: "80%",
+        marginTop: -8,
+    },
+    pic5: {
+        flex: 1,
+        resizeMode: 'contain',
+        width: "80%",
+        marginTop: -22,
+    },
+});
