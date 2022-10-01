62594791453b52293f592f1ad5e28a389aab51c4
jiacian0609 <79597797+jiacian0609@users.noreply.github.com>
2022-10-01 20:08:49 +0800

    style: 熟成階段

--- screens/KnowledgeScreen.tsx
+++ screens/KnowledgeScreen.tsx
@@ -3,30 +3,25 @@ import { StatusBar } from 'expo-status-bar';
 import { Ionicons } from '@expo/vector-icons';
 import { ScrollView, SafeAreaView, StyleSheet, Dimensions, Animated, TouchableOpacity, Button, Text, View, Image, ImageBackground } from 'react-native';
 import { useNavigation, NavigationContainer } from '@react-navigation/native';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import RNAnimatedScrollIndicators from '../node_modules/react-native-animated-scroll-indicators';
+import RNAnimatedScrollIndicators from 'react-native-animated-scroll-indicators';
+import Unorderedlist from 'react-native-unordered-list';
+
 import { api } from '../api';
 import Notification from './Notification';
 import { Context } from '../contexts/Context';
 
-// interface Props {
-//   isModalVisible: boolean;
-//   setModalVisible: React.Dispatch<React.SetStateAction<boolean>>;
-// }
-
 const { width } = Dimensions.get('window');
+
 function GoToEat({ screenName, isModalVisible, changeModalState }: any) {
   const navigation = useNavigation();
 
   return (
-    <TouchableOpacity onPress={() => navigation.navigate(screenName, {isModalVisible: isModalVisible, changeModalState: changeModalState})} style={styles.Button_E}>
-      <View>
-        <ImageBackground style={styles.banana_K} source={require('../assets/images/香蕉熟成階段.png')}>
-          <Text style={styles.text_K}> 香蕉熟成階段 </Text>
-        </ImageBackground>
-      </View>
+    <TouchableOpacity onPress={() => navigation.navigate(screenName, {isModalVisible: isModalVisible, changeModalState: changeModalState})} style={styles.knowledgeButton}>
+      <ImageBackground style={styles.knowledgeButtonImage} source={require('../assets/images/香蕉熟成階段.png')}>
+        <Text style={styles.knowledgeButtonText}>香蕉熟成階段</Text>
+      </ImageBackground>
     </TouchableOpacity>
   );
 } 
@@ -34,22 +29,18 @@ function GoToRecipe({ screenName, isModalVisible, changeModalState }: any) {
   const navigation = useNavigation();
 
   return (
-    <TouchableOpacity onPress={() => navigation.navigate(screenName, {isModalVisible: isModalVisible, changeModalState: changeModalState})} style={styles.Button_R}>
-      <View>
-        <ImageBackground style={styles.banana_K} source={require('../assets/images/廚房用具.png')}>
-          <Text style={styles.text_K}> 香蕉食譜 </Text>
-        </ImageBackground>
-      </View>
+    <TouchableOpacity onPress={() => navigation.navigate(screenName, {isModalVisible: isModalVisible, changeModalState: changeModalState})} style={styles.knowledgeButton}>
+      <ImageBackground style={styles.knowledgeButtonImage} source={require('../assets/images/廚房用具.png')}>
+        <Text style={styles.knowledgeButtonText}>香蕉食譜</Text>
+      </ImageBackground>
     </TouchableOpacity>
   );
 }
 
 function KnowledgeScreen() {
-  // const insets = useSafeAreaInsets();
   const { isModalVisible, changeModalState } = useContext(Context);
-  // const [isModalVisible, setModalVisible] = React.useState<boolean>(false);
   const openNotification = () => {
-    changeModalState(true);
+    changeModalState();
   };
 
   return (
@@ -57,17 +48,17 @@ function KnowledgeScreen() {
       <StatusBar />
         {/* <SafeAreaView style={styles.container_K}> */}
         {/* <View style={[styles.container_K, { paddingTop: Math.max(insets.top, 16) }]}> */}
-        <View style={styles.container_K}>
-          <View style={styles.header_K}>
-            <View style={styles.first_K}>
-              <Text style={styles.title_K}> 關於香蕉 </Text>
+        <View style={styles.container}>
+          <View style={styles.header}>
+            <View style={styles.first}>
+              <Text style={styles.title}> 關於香蕉 </Text>
               <TouchableOpacity>
                 <Ionicons name="notifications-outline" size={25} style={styles.notification_K} onPress={openNotification}/>
               </TouchableOpacity>
               <Notification isModalVisible={isModalVisible} changeModalState={changeModalState}/>
             </View>
           </View>
-          <View style={styles.main_K}>
+          <View style={styles.main}>
             <GoToEat screenName="香蕉熟成階段" isModalVisible={isModalVisible} changeModalState={changeModalState} />
             <GoToRecipe screenName="香蕉食譜" isModalVisible={isModalVisible} changeModalState={changeModalState} />
           </View>
@@ -92,56 +83,60 @@ function EatScreen({navigation, route}: any) {
         {/* <View style={[styles.container, { paddingTop: Math.max(insets.top, 16) }]}> */}
         <View style={styles.container}>
           <View style={styles.header}>
-            <TouchableOpacity onPress={() => navigation.goBack()} style={styles.button_back}>
-              <View>
-                <Text style={styles.text_back}> ᐸ  返回 </Text>
-              </View>
-            </TouchableOpacity>
             <View style={styles.first}>
-              <Text style={styles.title}> 香蕉熟成階段 </Text>
+              <TouchableOpacity onPress={() => navigation.goBack()} style={styles.button_back}>
+                  <Text style={styles.text_back}>ᐸ  返回</Text>
+              </TouchableOpacity>
+              <Text style={styles.title}>香蕉熟成階段</Text>
                 <TouchableOpacity>
                   <Ionicons name="notifications-outline" size={25} style={styles.notification} onPress={openNotification}/>
                 </TouchableOpacity>
             </View>
           </View>
           <ScrollView>
-            <View>
-              <View style={styles.middle}>
-                <Text style={styles.header_text} />
-                <Text style={styles.header_text}>  第一階段   墨綠色{"\n"}</Text>
-                <Image style={styles.banana} source={require('../assets/images/banana_1.png')} />
-                <Text style={styles.header_text} />
-                <Text style={styles.text}>
-                ✦ 未熟，不宜食用{"\n"}
-                </Text>
+            <View style={styles.middle}>
+              <Text style={styles.header_text}>第一階段 - 墨綠色</Text>
+              <Image style={styles.bananaImage} source={require('../assets/images/banana_1.png')} />
+              <Unorderedlist bulletUnicode={0x2726} color='#7E6107' style={{ fontSize: 18, lineHeight: 43, marginBottom: 30 }}>
+                <Text style={styles.text}>未熟，不宜食用</Text>
+              </Unorderedlist>
 
-                <Text style={styles.header_text}>  第二階段   淡青色至半青半黃{"\n"}</Text>
-                <Image style={styles.banana} source={require('../assets/images/banana_2.png')} />
-                <Text style={styles.header_text} />
-                <Text style={styles.text}>
-                ✦ 口感苦澀、皮厚肉硬{"\n"}
-                ✦ 含有「難消化性麥芽糊精」，不易被小腸吸收，升糖指數較低，可穩定血糖，有助於改善腸道健康{"\n"}
-                ✦ 具有抗性澱粉、提供高飽足感，但不易消化，吃多容易引起腹脹、消化不良等反應{"\n"}
-                </Text>
-
-                <Text style={styles.header_text}>  第三階段   四分之三至全黃香蕉{"\n"}</Text>
-                <Image style={styles.banana} source={require('../assets/images/banana_3.png')} />
-                <Text style={styles.header_text} />
-                <Text style={styles.text}>
-                ✦ 味道香濃、口感軟滑{"\n"}
-                ✦ 含有維他命B2、B6及C，促進新陳代謝，養顏美白效果{"\n"}
-                ✦ 豐富的鉀和鎂能消除疲勞，減少運動抽筋的機會。助消化、安眠、抗憂鬱{"\n"}
-                </Text>
-
-                <Text style={styles.header_text}>  第四階段   帶有啡點之全熟香蕉{"\n"}</Text>
-                <Image style={styles.banana} source={require('../assets/images/banana_4.png')} />
-                <Text style={styles.header_text} />
-                <Text style={styles.text}>
-                ✦ 口感軟爛，適合牙口不好者食用{"\n"}
-                ✦ 含有大量多酚，能夠延緩衰老{"\n"}
-                ✦ 啡點越多，免疫活性越高，當中的磷脂質有助抑制胃潰瘍，幫助消化{"\n"}
-                </Text>
-              </View>
+              <Text style={styles.header_text}>第二階段 - 淡青色至半青半黃</Text>
+              <Image style={styles.bananaImage} source={require('../assets/images/banana_2.png')} />
+              <Unorderedlist bulletUnicode={0x2726} color='#7E6107' style={{ fontSize: 18, lineHeight: 43 }}>
+                <Text style={styles.text}>口感苦澀、皮厚肉硬</Text>
+              </Unorderedlist>
+              <Unorderedlist bulletUnicode={0x2726} color='#7E6107' style={{ fontSize: 18, lineHeight: 43 }}>
+                <Text style={styles.text}>含有「難消化性麥芽糊精」，不易被小腸吸收，升糖指數較低，可穩定血糖，有助於改善腸道健康</Text>
+              </Unorderedlist>
+              <Unorderedlist bulletUnicode={0x2726} color='#7E6107' style={{ fontSize: 18, lineHeight: 43, marginBottom: 80 }}>
+                <Text style={styles.text}>具有抗性澱粉、提供高飽足感，但不易消化，吃多容易引起腹脹、消化不良等反應</Text>
+              </Unorderedlist>
+              
+              <Text style={styles.header_text}>第三階段 - 四分之三至全黃香蕉</Text>
+              <Image style={styles.bananaImage} source={require('../assets/images/banana_3.png')} />
+              <Unorderedlist bulletUnicode={0x2726} color='#7E6107' style={{ fontSize: 18, lineHeight: 43 }}>
+                <Text style={styles.text}>味道香濃、口感軟滑</Text>
+              </Unorderedlist>
+              <Unorderedlist bulletUnicode={0x2726} color='#7E6107' style={{ fontSize: 18, lineHeight: 43 }}>
+                <Text style={styles.text}>含有維他命B2、B6及C，促進新陳代謝，養顏美白效果</Text>
+              </Unorderedlist>
+              <Unorderedlist bulletUnicode={0x2726} color='#7E6107' style={{ fontSize: 18, lineHeight: 43, marginBottom: 80 }}>
+                <Text style={styles.text}>豐富的鉀和鎂能消除疲勞，減少運動抽筋的機會。助消化、安眠、抗憂鬱</Text>
+              </Unorderedlist>
+            
+              <Text style={styles.header_text}>第四階段 - 帶有啡點之全熟香蕉</Text>
+              <Image style={styles.bananaImage} source={require('../assets/images/banana_4.png')} />
+              <Unorderedlist bulletUnicode={0x2726} color='#7E6107' style={{ fontSize: 18, lineHeight: 43 }}>
+                <Text style={styles.text}>口感軟爛，適合牙口不好者食用</Text>
+              </Unorderedlist>
+              <Unorderedlist bulletUnicode={0x2726} color='#7E6107' style={{ fontSize: 18, lineHeight: 43 }}>
+                <Text style={styles.text}>含有大量多酚，能夠延緩衰老</Text>
+              </Unorderedlist>
+              <Unorderedlist bulletUnicode={0x2726} color='#7E6107' style={{ fontSize: 18, lineHeight: 43 }}>
+                <Text style={styles.text}>啡點越多，免疫活性越高，當中的磷脂質有助抑制胃潰瘍，幫助消化</Text>
+              </Unorderedlist>
+              
             </View>
           </ScrollView>
           <Notification isModalVisible={isModalVisible} changeModalState={changeModalState}/>
@@ -267,25 +262,25 @@ export default function App() {
 } 
 
 const styles = StyleSheet.create({
-  container_K: {
+  container: {
     flex: 1, //佔據所有空間
     backgroundColor: '#fff',
   },
-  header_K: {
+  header: {
     flexDirection: 'row',
     backgroundColor: '#fff',
     justifyContent: 'center',
   },
-  first_K: {
+  first: {
     height: 210,
-    width: 420,
+    width: '100%',
     backgroundColor: '#FAE5A4',
     borderBottomLeftRadius: 60,
     borderBottomRightRadius: 60,
     paddingTop: 160,
     flexDirection: 'row',
   },
-  title_K: {
+  title: {
     fontSize: 25,
     color: "#7E6107",
     fontWeight: 'bold',
@@ -297,68 +292,57 @@ const styles = StyleSheet.create({
     paddingTop: 3,
     paddingLeft: 147.5,
   },
-  main_K: {
+  main: {
     flex: 1,
-    width: '100%',
-    height: '100%',
     backgroundColor: '#fff',
     alignItems: 'center',
+    justifyContent: 'space-evenly',
   }, 
-  Button_E: {
-    marginTop: 70,
+  knowledgeButton: {
+    // marginTop: 70,
     height: 180,
     width: '50%',
     alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'transparent',
-  },
-  Button_R: {
-    height: 10,
-    width: '50%',
-    paddingBottom: 150,
-    marginTop: 60,
-    marginBottom: 100,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'transparent',
-  },
-  banana_K: {
     borderRadius: 20,
+    justifyContent: 'center',
+    backgroundColor: '#000',
+  },
+  knowledgeButtonImage: {
     width: 216,
     height: 202,
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: '#fff',
   },
-  text_K: {
+  knowledgeButtonText: {
     fontSize: 27,
     color: "#fff",
     fontWeight: 'bold',
   },
 
   // EatScreen
-  container: {
-    flex: 1, //佔據所有空間
-    backgroundColor: '#fff',
-  },
-  header: {
-    height: 210,
-    width: 420,
-    backgroundColor: '#FAE5A4',
-    borderBottomLeftRadius: 60,
-    borderBottomRightRadius: 60,
-    marginLeft: -4,
-  },
-  first: {
-    flexDirection: 'row',
-    paddingTop: 80,
-  },
-  title: {
-    fontSize: 25,
-    color: "#7E6107",
-    fontWeight: 'bold',
-    paddingLeft: 60,
-  },
+  // container: {
+  //   flex: 1, //佔據所有空間
+  //   backgroundColor: '#fff',
+  // },
+  // header: {
+  //   height: 210,
+  //   width: 420,
+  //   backgroundColor: '#FAE5A4',
+  //   borderBottomLeftRadius: 60,
+  //   borderBottomRightRadius: 60,
+  //   marginLeft: -4,
+  // },
+  // first: {
+  //   flexDirection: 'row',
+  //   paddingTop: 80,
+  // },
+  // title: {
+  //   fontSize: 25,
+  //   color: "#7E6107",
+  //   fontWeight: 'bold',
+  //   paddingLeft: 60,
+  // },
   notification: {
     color: "#7E6107",
     fontWeight: 'bold',
@@ -366,39 +350,37 @@ const styles = StyleSheet.create({
     paddingLeft: 94.5,
   },
   button_back: {
-    height: 50,
-    width: 200,
-    marginTop: 30,
-    justifyContent: 'center',
-    marginLeft: -30,
+    position: 'absolute',
+    top: 60,
+    left: 60,
+    zIndex: 1,
   },
   text_back: {
     fontSize: 18,
     color: "#7E6107",
-    marginLeft: 70,
-    marginTop: 25,
   },
   middle: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingVertical: 30,
+    paddingHorizontal: 55,
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
   header_text: {
+    marginBottom: 10,
     fontSize: 20,
     fontWeight: 'bold',
     color: "#4D3604",
-    marginLeft: -120,
   },
   text: {
     fontSize: 18,
     color: "#7E6107",
-    marginLeft: 100,
-    marginRight: 90,
+    marginVertical: 10,
   },
-  banana: {
+  bananaImage: {
     flex: 1,
     borderRadius: 20,
-    width: "75%",
+    width: "100%",
     height: 180,
     alignItems: 'center',
     justifyContent: 'center',
